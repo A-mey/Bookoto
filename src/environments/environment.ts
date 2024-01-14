69031b4f4696ba625b44e773ece1634cb88c7529
Ameya <amey2p@gmail.com>
2024-01-15 03:42:31 +0530

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-    production: false,
-    sendRegistrationRequestUrl: 'localhost:2004/login/registration',
-    sendLoginRequestUrl: 'localhost:2004/login/login',
-    sendOtpRequest: 'localhost:2004/otp/otp',
-    sendVerifyOtpRequestUrl: 'localhost:2004/otp/verification',
+    production: true,
+    sendRegistrationRequestUrl: 'http://localhost:2004/login/registration',
+    sendLoginRequestUrl: 'http://localhost:2004/login/login',
+    sendOtpRequest: 'http://localhost:2004/otp/otp',
+    sendVerifyOtpRequestUrl: 'http://localhost:2004/otp/verification',
 };
